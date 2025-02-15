ea321450b7705cef5f2ae7ab6e319efe97a8a2c5
bp848 <ishijima@b-p.co.jp>
2025-02-15 16:58:39 +0900

    fix: update stripe api version to 2025-01-27.acacia

--- src/server/service/stripe.service.ts
+++ src/server/service/stripe.service.ts
@@ -9,7 +9,7 @@ import { metadata } from '@/app/layout';
 
 export const stripe = new Stripe(env.STRIPE_SECRET_KEY, {
   typescript: true,
-  apiVersion: '2024-09-30.acacia',
+  apiVersion: '2025-01-27.acacia',
 });
 
 /**
